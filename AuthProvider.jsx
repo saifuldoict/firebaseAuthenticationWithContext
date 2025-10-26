320835bf2fe797d0be17fa49ca79f27420e50b84
Md. Saiful Islam <saiful.doict@gmail.com>
2025-10-26 11:12:35 +0600

    Update AuthProvider.jsx

--- AuthProvider.jsx
+++ AuthProvider.jsx
@@ -1,13 +1,15 @@
 import React, { createContext, useState, useEffect } from "react";
 import {getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,updateProfile,signOut,GoogleAuthProvider,signInWithPopup,onAuthStateChanged,sendPasswordResetEmail,} from "firebase/auth";
 import app from "../firebase/firebase.config";
-export const AuthContext = createContext();
 
 
-const auth = getAuth(app);
+export const AuthContext = createContext();  // কনটেক্সট এর জন্য প্রথম কাজ
+
+
+const auth = getAuth(app);  // fiebbase  এর কোড
 const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = ({ children }) => {
+const AuthProvider = ({ children }) => {   //২য় কাজ main.jsx ফাইলের Router এর ভিতরের সকল ফাইল একসেস এর জন্য children
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -20,7 +22,7 @@ const AuthProvider = ({ children }) => {
     return () => unsubscribe();
   }, []);
 
- 
+ // ইমেইল-পাসওয়ার্ড দিয়ে রেজিস্টেশন এর কোড
   const registerUser = async (name, email, password, photoURL) => {
     setLoading(true);
     try {
@@ -33,7 +35,7 @@ const AuthProvider = ({ children }) => {
     }
   };
 
-
+ // ইমেইল-পাসওয়ার্ড দিয়ে লগইন এর কোড
   const loginUser = async (email, password) => {
     setLoading(true);
     try {
@@ -45,7 +47,7 @@ const AuthProvider = ({ children }) => {
     }
   };
 
-
+ // গুগল দিয়ে রেজিস্টেশন এর কোড
   const loginWithGoogle = async () => {
     setLoading(true);
     try {
@@ -57,7 +59,7 @@ const AuthProvider = ({ children }) => {
     }
   };
 
-
+ // লগআউট এর কোড
   const logOut = async () => {
     setLoading(true);
     try {
@@ -68,14 +70,14 @@ const AuthProvider = ({ children }) => {
     }
   };
 
-
+ // ইউজার আপডেট এর কোড
   const updateUserProfile = async (profile) => {
     if (!auth.currentUser) throw new Error("No user is logged in");
     await updateProfile(auth.currentUser, profile);
     setUser({ ...auth.currentUser });
   };
 
-
+ // পাসওয়ার্ড রিসেট এর কোড
   const resetPassword = async (email) => {
     if (!email) throw new Error("Please provide an email");
     await sendPasswordResetEmail(auth, email);
@@ -83,10 +85,10 @@ const AuthProvider = ({ children }) => {
   };
 
 
-  const authInfo = {user,loading,registerUser,loginUser,loginWithGoogle,logOut, updateUserProfile,resetPassword,};
+  const authInfo = {user,loading,registerUser,loginUser,loginWithGoogle,logOut, updateUserProfile,resetPassword,};     // ৪র্থ কাজ অবজেক্ট তৈরি করা। উপরের যত ফাংশন করা হবে সবকিছু এই অবজেক্টএর ভিতরে রাখতে হবে।
 
   return (
-    <AuthContext.Provider value={authInfo}>
+    <AuthContext.Provider value={authInfo}>      // ৩য় কাজ ভেলুর ভিতরে যা যা থাকবে সেগুল অন্য যেকোন ফাইল থেকে একসেস করা যাবে
       {!loading && children}
     </AuthContext.Provider>
   );
